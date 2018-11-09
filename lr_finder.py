b0d475fb092d242be0535bc884405662e8f757db
rohitgr7 <rohitgr1998@gmail.com>
2018-11-10 00:04:14 +0530

    PEP8

--- lr_finder.py
+++ lr_finder.py
@@ -1,14 +1,11 @@
-## TODO ##
-#  Fix plot_dlos()
-##########
-
-from tensorflow.keras.callbacks import LambdaCallback, Callback
+from tensorflow.keras.callbacks import LambdaCallback
 from tensorflow.keras import backend as K
 import matplotlib.pyplot as plt
 import math
 
 
 class LRFinder:
+
     def __init__(self, model):
         self.model = model
         self.lrs = []
@@ -46,8 +43,10 @@ class LRFinder:
 
         # Train with callback
         K.set_value(self.model.optimizer.lr, start_lr)
-        callback = LambdaCallback(on_batch_end=lambda batch, logs: self.on_batch_end(batch, logs))
-        self.model.fit(X, y, batch_size=batch_size, epochs=epochs, callbacks=[callback])
+        callback = LambdaCallback(
+            on_batch_end=lambda batch, logs: self.on_batch_end(batch, logs))
+        self.model.fit(X, y, batch_size=batch_size,
+                       epochs=epochs, callbacks=[callback])
 
         # Set original weights and le
         K.set_value(self.model.optimizer.lr, org_lr)
@@ -59,19 +58,22 @@ class LRFinder:
         plt.ylabel('Learning rate')
 
     def plot_loss(self, n_skip_start=10, n_skip_end=5):
-        plt.plot(self.lrs[n_skip_start:-(n_skip_end + 1)], self.losses[n_skip_start:-(n_skip_end + 1)])
+        plt.plot(self.lrs[n_skip_start:-(n_skip_end + 1)],
+                 self.losses[n_skip_start:-(n_skip_end + 1)])
         plt.xlabel('Learning rate (log scale)')
         plt.ylabel('Loss')
         plt.xscale('log')
 
-    def plot_dloss(self, sma=1, n_skip_start=10, n_skip_end=5, y_lim=(-1e-2, 1e-2)):
+    def plot_dloss(self, sma=1, n_skip_start=10, n_skip_end=5,
+                   y_lim=(-0.01, 0.01)):
         derivatives = [0.] * (sma)
 
         for i in range(sma, len(self.lrs)):
             derivative = (self.losses[i] - self.losses[i - sma]) / sma
             derivatives.append(derivative)
 
-        plt.plot(self.lrs[n_skip_start:-(n_skip_end + 1)], derivatives[n_skip_start:-(n_skip_end + 1)])
+        plt.plot(self.lrs[n_skip_start:-(n_skip_end + 1)],
+                 derivatives[n_skip_start:-(n_skip_end + 1)])
         plt.xlabel("Learning rate (log scale)")
         plt.ylabel("dLoss")
         plt.xscale('log')
